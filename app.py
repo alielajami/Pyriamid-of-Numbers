97fbcc0b9a83038e258e8174760c2916b7dcf9e3
Ali El Ajami <161844424+alielajami@users.noreply.github.com>
2024-10-27 16:15:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 x = int(input("Enter a number: "))
 
-if x < 1:
+if x < 1: # Error handeling
     print("x should be greater than 0")
-else:
-    for i in range(x):
-        for j in range(i+1):
+else: # Start the pyramid app
+    for i in range(x): # For rows
+        for j in range(i+1): # For actual numbers on each row
             print(j+1, end=" ")
         print()
